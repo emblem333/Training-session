1c3f105a460ba34b82473b5a17cd4736f1f75595
emblem333 <122144556+emblem333@users.noreply.github.com>
2024-03-02 23:54:45 +0900

    gsap add

--- 20240210/課題/js/script_assignment.js
+++ 20240210/課題/js/script_assignment.js
@@ -1,9 +1,9 @@
-gsap.to('.animation_1',{x:"90vw", rotation:720, duration:2,
+gsap.to('.animation_1',{x:"90vw", rotation:720, duration:3,
   scrollTrigger:{
     trigger:'.animation_1',
-    start:'top 50%',
+    start:'top 40%',
     end:'bottom 30%',
-    markers:true,
+    // markers:true,
     toggleActions:'play none none reverse',
     repeat: -1,
   }
@@ -11,7 +11,7 @@ gsap.to('.animation_1',{x:"90vw", rotation:720, duration:2,
 
 gsap.fromTo('.animation_2',
   {
-    y: 100, /*アニメーション開始前の縦位置(下に100px)*/
+    y: 300, /*アニメーション開始前の縦位置(下に100px)*/
     autoAlpha: 0, /*アニメーション開始前は透明*/
   },
   {
@@ -19,17 +19,17 @@ gsap.fromTo('.animation_2',
     autoAlpha: 1, // アニメーション後に出現(透過率0)
     scrollTrigger:{
       trigger:'.animation_2',
-      start:'top 50%',
+      start:'top 60%',
       end:'bottom 30%',
       toggleActions:'play none none reverse',
     },
   }
 );
 
-gsap.to('.animation_3',{x:400,
+gsap.to('.animation_3',{scaleX:3, scaleY:3,
   scrollTrigger:{
     trigger:'.animation_3',
-    start:'top 40%',
+    start:'top 50%',
     end:'bottom 30%',
     toggleActions:'play none none reverse',
   }
