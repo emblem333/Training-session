0d0f16af332b619fe434d00c7762b49986facc12
emblem333 <122144556+emblem333@users.noreply.github.com>
2024-03-02 23:16:13 +0900

    gsap add

--- 20240210/課題/js/script_assignment.js
+++ 20240210/課題/js/script_assignment.js
@@ -1,20 +1,31 @@
-gsap.to('.animation_1',{x:400,
+gsap.to('.animation_1',{x:"90vw", rotation:720, duration:2,
   scrollTrigger:{
     trigger:'.animation_1',
     start:'top 50%',
     end:'bottom 30%',
     markers:true,
     toggleActions:'play none none reverse',
+    repeat: -1,
   }
 });
-gsap.to('.animation_2',{x:400,
-  scrollTrigger:{
-    trigger:'.animation_2',
-    start:'top 50%',
-    end:'bottom 30%',
-    toggleActions:'play none none reverse',
+
+gsap.fromTo('.animation_2',
+  {
+    y: 100, /*アニメーション開始前の縦位置(下に100px)*/
+    autoAlpha: 0, /*アニメーション開始前は透明*/
+  },
+  {
+    y: 0, // アニメーション後の縦位置(上に100px)
+    autoAlpha: 1, // アニメーション後に出現(透過率0)
+    scrollTrigger:{
+      trigger:'.animation_2',
+      start:'top 50%',
+      end:'bottom 30%',
+      toggleActions:'play none none reverse',
+    },
   }
-});
+);
+
 gsap.to('.animation_3',{x:400,
   scrollTrigger:{
     trigger:'.animation_3',
